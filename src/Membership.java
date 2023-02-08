b164c127cd43620ba08ad2adfeec3d685424ed57
Wenxuye6 <122643147+Wenxuye6@users.noreply.github.com>
2023-02-08 16:12:20 -0500

    Update Membership.java

--- src/Membership.java
+++ src/Membership.java
@@ -1,16 +1,22 @@
 package membership;
-import java.util.ArrayList;
-import course.java;
+
+import java.util.Scanner;
 
 public class membership {
 
 	
 	private String name;
+	private int answer;
 	private String gender;
 	private int age;
-	private int remainFund;
+	private int number;
+	private int addMoney;
+
+
+	private int remainFund=0;
 	private int bodyindex;
-	private ArrayList<course> courses;
+	private int price;
+	private int fund;
 	
 	
 	public int getRemainFund() {
@@ -32,24 +38,66 @@ public class membership {
 		this.name = name;
 		this.gender = gender;
 		this.age = age;
+		this.answer=answer;
 		this.remainFund = remainFund;
 		this.bodyindex = bodyindex;
-		this.courses = new ArrayList<course>();
-	}
-	
-	public void addcourse(String name, int price, String desc){
-		courses.add(new course(name, price, desc));
-	
+		this.price=price;
 		
-	}	
-	
-	public void showEverything() {
-		System.out.println( "membership [name=" + name + ", gender=" + gender + ", age=" + age + ", remainFund=" + remainFund
-				+ ", bodyindex=" + bodyindex + "]");
 	}
 	
+
+
+	
+//type the personal information;	
+	public void showInformation() {
+		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+	    System.out.println("Enter username");
+	    name = myObj.nextLine();  // Read user input
+	    System.out.println("Enter gender");
+	    gender = myObj.nextLine();
+	    Scanner myInput = new Scanner( System.in );
+	    System.out.println( "Enter age " );
+	    age = myInput.nextInt();
+	    
+	    Scanner Input = new Scanner( System.in );
+	    System.out.print( "The remainFund for your account is"+ remainFund + "Do you want add more money? 1 for YES, 0 For NO");
+	    answer = Input.nextInt();
+	    if (answer==1) {
+	    	Scanner add = new Scanner( System.in );
+	    	System.out.println( "how much you want to add?" );
+		    addMoney = add.nextInt();
+		    remainFund=remainFund +addMoney;
+	    }
+	    if (answer==0) {
+	    	System.out.println( "loser" );
+	    }else {
+	    	System.out.println( "Please follow the instruction!" );
+	    }
+
+	}
+	
+//check the remainFund for different age person;
+	public int fundCheck() {
+			
+			if(age<18){
+				price=25;
+			}
+			else {
+				if (age>55) {
+					price=15;
+					}
+					else {
+						price=35;
+						}
+			}
+		fund = remainFund-price;
+		return fund;
+		}
+
+	public void showEverything() {
+		System.out.println( "membership [name=" + name + ", gender=" + gender + ", age=" + age + ", remainFund=" +  fundCheck() + ", bodyindex=" + bodyindex + "]");
+	}
 	
 	
 	
 }
-
