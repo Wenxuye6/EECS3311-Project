f6fdf14a7a91945b8ef2b6df3f13afde281c544c
216870297 <124620794+216870297@users.noreply.github.com>
2023-02-08 18:45:10 -0500

    Update testInventory.java

--- src/testInventory.java
+++ src/testInventory.java
@@ -1,1 +1,36 @@
+package Inventory;
+
+public class Test {
+	
+
+	
+
+	 public static void main(String[] args) {
+		 
+		 dumbbell dumb1 = new dumbbell("50kg", 200.5);
+		 
+		 dumbbell dumb2 = new dumbbell("1000kg", 200.5);
+		 
+		 
+		 Inventory wenxudejia = new Inventory();
+		 wenxudejia.adddumbbell(dumb1);
+		 wenxudejia.adddumbbell(dumb2);
+		 wenxudejia.printdumbbell();	
+		 
+		 wenxudejia.remove(dumb2);
+		 wenxudejia.printdumbbell();
+		
+		 
+		 
+		 yogaMat yogaMat1 = new yogaMat("large", 100.0);
+		 yogaMat yogaMat2 = new yogaMat("small", 200.0);
+		 wenxudejia.adddyogaMat(yogaMat1);
+		 wenxudejia.adddyogaMat(yogaMat2);
+		wenxudejia.remove(yogaMat1);
+		 wenxudejia.printyogaMat();
+	  
+	 }
+
+	
+}
 
