1abad6375d410a2b003fcab6995edd987075f73c
Zhige Xia <122643293+Ericxiazhige@users.noreply.github.com>
2023-03-09 14:15:51 -0500

    Update gym.sql

--- gym.sql
+++ gym.sql
@@ -24,18 +24,39 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `t_admin`;
 CREATE TABLE `t_admin`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '管理员ID',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'Administrator ID',
   `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Administrator account',
   `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Administrator password',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of t_admin
+-- ----------------------------
+INSERT INTO `t_admin` VALUES (1, 'admin', '123456');
+
+-- ----------------------------
+-- Table structure for t_course
+-- ----------------------------
+DROP TABLE IF EXISTS `t_course`;
+CREATE TABLE `t_course`  (
+  `courseId` int(0) NOT NULL AUTO_INCREMENT,
+  `courseName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `price` double(10, 2) NULL DEFAULT NULL,
+  `benefit` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`courseId`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_course
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for t_member
 -- ----------------------------
 DROP TABLE IF EXISTS `t_member`;
 CREATE TABLE `t_member`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'User ID',
   `account` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User name',
   `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User Password',
   `realName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Name',
@@ -48,5 +69,40 @@ CREATE TABLE `t_member`  (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Records of t_member
+-- ----------------------------
+-- ----------------------------
+-- Table structure for t_schedule
+-- ----------------------------
+DROP TABLE IF EXISTS `t_schedule`;
+CREATE TABLE `t_schedule`  (
+  `scheduleId` int(0) NOT NULL AUTO_INCREMENT,
+  `memberName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `courseName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `coachName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`scheduleId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_schedule
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_work
+-- ----------------------------
+DROP TABLE IF EXISTS `t_work`;
+CREATE TABLE `t_work`  (
+  `workId` int(0) NOT NULL AUTO_INCREMENT,
+  `memberName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `work` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `identity` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`workId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_work
+-- ----------------------------
+
 SET FOREIGN_KEY_CHECKS = 1;
-insert into t_admin (username,password) values('admin',123456)
+
