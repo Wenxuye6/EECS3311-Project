54b649f1eb8a8bbd1fff40cd7000482943eefa7d
Zhige Xia <122643293+Ericxiazhige@users.noreply.github.com>
2023-03-09 14:44:45 -0500

    Update MainPanel.java

--- src/view/panel/MainPanel.java
+++ src/view/panel/MainPanel.java
@@ -2,19 +2,22 @@ package view.panel;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 
 /**
  * Welcome Panel
  */
 public class MainPanel extends JPanel {
 
-    public MainPanel(String username) {
+    public MainPanel(String username, String identity) {
         setLayout(null);
-        Label label = new Label("Welcome "+ username +" to use GymManage System");
+        Label label = new Label("Welcome ' " + username + " ' to use GymManage System");
+        Label label1 = new Label("Your identity is ' " + identity + " '");
 
         label.setFont(new Font("", Font.BOLD, 20));
-        label.setBounds(100,50,650,40);
+        label1.setFont(new Font("", Font.BOLD, 20));
+        label.setBounds(100, 50, 650, 40);
+        label1.setBounds(100, 100, 650, 40);
         add(label);
+        add(label1);
     }
 }
