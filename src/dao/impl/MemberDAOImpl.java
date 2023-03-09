1fc7dd243b7112beb93e635de9b9704073a20e17
Zhige Xia <122643293+Ericxiazhige@users.noreply.github.com>
2023-03-09 18:42:49 -0500

    Update MemberDAOImpl.java

--- src/dao/impl/MemberDAOImpl.java
+++ src/dao/impl/MemberDAOImpl.java
@@ -17,34 +17,42 @@ public class MemberDAOImpl extends BaseDAO<Member> implements MemberDAO {
     }
 
     @Override
-    public boolean register(Member Member) {
-        if (isExists(Member.getAccount())) {
+    public boolean addMember(Member member) {
+        if (isExists(member.getAccount())) {
             return false;
         }
-        String sql = "insert into `t_member`(`account`,`password`)values(?,?)";
-        return update(sql, Member.getAccount(), Member.getPassword()) > 0;
-    }
-
-    @Override
-    public boolean addMember(Member Member) {
-        if (isExists(Member.getAccount())) {
-            return false;
-        }
-        String sql = "insert into `t_member`(`account`,`password`,`realName`,`gender`,`height`,`weight`,`BMI`,`Fund`,`VIP`)values(?,?,?,?,?,?,?,?,?)";
-        return update(sql, Member.getAccount(), Member.getPassword(), Member.getRealName(), Member.getGender(), Member.getHeight(),
-                Member.getWeight(), Member.getBMI(), Member.getFund(), Member.getVIP()) > 0;
+        String sql = "insert into `t_member`(`account`,`password`,`realName`,`gender`,`height`,`weight`,`BMI`,`Fund`,`identity`)values(?,?,?,?,?,?,?,?,?)";
+        return update(sql, member.getAccount(), member.getPassword(), member.getRealName(), member.getGender(), member.getHeight(),
+                member.getWeight(), member.getBMI(), member.getFund(), member.getIdentity()) > 0;
     }
 
     @Override
     public void change(Member newMember) {
-        String sql = "update `t_member` set `password`=? where `account`=?";
-        update(sql, newMember.getPassword(), newMember.getAccount());
+        String sql = "update `t_member` set account=?,`password`=?,realName=?,gender=?,height=?,weight=?,BMI=?,Fund=?,identity=? where `memberId`=?";
+        update(sql, newMember.getAccount(), newMember.getPassword(), newMember.getRealName(), newMember.getGender(), newMember.getHeight(),
+                newMember.getWeight(), newMember.getBMI(), newMember.getFund(), newMember.getIdentity(), newMember.getMemberId());
     }
 
     @Override
-    public void changeMember(Member newMember) {
-        String sql = "update `t_member` set account=?,`password`=?,realName=?,gender=?,height=?,weight=?,BMI=?,Fund=?,VIP=? where `id`=?";
-        update(sql, newMember.getAccount(), newMember.getPassword(), newMember.getRealName(), newMember.getGender(), newMember.getHeight(), newMember.getWeight(), newMember.getBMI(), newMember.getFund(), newMember.getVIP(), newMember.getId());
+    public boolean changeMember(Member newMember) {
+        if (isExists(newMember.getAccount())) {
+            return false;
+        }
+        String sql = "update `t_member` set account=?,`password`=?,realName=?,gender=?,height=?,weight=?,BMI=?,Fund=?,identity=? where `memberId`=?";
+        return update(sql, newMember.getAccount(), newMember.getPassword(), newMember.getRealName(), newMember.getGender(), newMember.getHeight(),
+                newMember.getWeight(), newMember.getBMI(), newMember.getFund(), newMember.getIdentity(), newMember.getMemberId()) > 0;
+    }
+
+    @Override
+    public void deleteMemberById(int id) {
+        String sql = "delete from `t_member` where `memberId`=?";
+        update(sql, id);
+    }
+
+    @Override
+    public String getIdentityByAccount(String account) {
+        String sql = "select `identity` from `t_member` where `account`=?";
+        return getBean(sql, account).getIdentity();
     }
 
     @Override
@@ -66,9 +74,21 @@ public class MemberDAOImpl extends BaseDAO<Member> implements MemberDAO {
     }
 
     @Override
-    public Member getMemberById(int id) {
-        String sql = "select * from `t_member` where `id`=?";
-        return getBean(sql, id);
+    public Object[][] getCoachArrayList() {
+        String sql = "select * from `t_member` where `identity`='coach'";
+        List<Object[]> l = getArrayList(sql);
+        Object[][] list = new Object[l.size()][10];
+        int i = 0;
+        for (Object[] o : l) {
+            list[i++] = o;
+        }
+        return list;
+    }
+
+    @Override
+    public Member getMemberByAccount(String account) {
+        String sql = "select * from `t_member` where `account`=?";
+        return getBean(sql, account);
     }
 
     @Override
