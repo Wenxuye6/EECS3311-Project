31d0615c88a148aa1f79ba5840b34d5323c9dce6
HaochengSun1 <124642587+HaochengSun1@users.noreply.github.com>
2023-03-09 22:38:27 -0500

    Update Schedule.java

--- src/bean/Schedule.java
+++ src/bean/Schedule.java
@@ -1,14 +1,18 @@
 package bean;
 
 public class Schedule {
+    //schedule fields
     private Integer scheduleId;
     private String memberName;
     private String courseName;
     private String coachName;
 
+    //constructers
     public Schedule() {
     }
 
+    
+    //constructers
     public Schedule(Integer scheduleId, String memberName, String courseName, String coachName) {
         this.scheduleId = scheduleId;
         this.memberName = memberName;
@@ -16,38 +20,47 @@ public class Schedule {
         this.coachName = coachName;
     }
 
+    //getters of ID
     public Integer getScheduleId() {
         return scheduleId;
     }
-
+ 
+    //setters of ID
     public void setScheduleId(Integer scheduleId) {
         this.scheduleId = scheduleId;
     }
 
+    //getters of name
     public String getMemberName() {
         return memberName;
     }
 
+    //setters of name
     public void setMemberName(String memberName) {
         this.memberName = memberName;
     }
 
+    //getters of name
     public String getCourseName() {
         return courseName;
     }
 
+    //setters of name
     public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
 
+    //getters coachname
     public String getCoachName() {
         return coachName;
     }
 
+    //setters coach name
     public void setCoachName(String coachName) {
         this.coachName = coachName;
     }
 
+    //print methods
     @Override
     public String toString() {
         return "Schedule{" +
