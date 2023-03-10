1beae980e8efcdc7bd820a1a174756c66b189b3c
HaochengSun1 <124642587+HaochengSun1@users.noreply.github.com>
2023-03-09 22:19:52 -0500

    Update Member.java

--- src/bean/Member.java
+++ src/bean/Member.java
@@ -4,6 +4,7 @@ package bean;
  * User entity classes
  */
 public class Member {
+    //fields of a member
     private Integer memberId;
     private String account; //User account
     private String password; //User password
@@ -15,14 +16,17 @@ public class Member {
     private Double Fund;
     private String identity;
 
+    //No-Arg Constructor.
     public Member() {
     }
 
+    //Parameterized Constructor.
     public Member(String account, String password) {
         this.account = account;
         this.password = password;
     }
 
+    //Parameterized Constructor.
     public Member(Integer memberId, String account, String password, String realName, String gender, Double height, Double weight, Double BMI, Double fund, String identity) {
         this.memberId = memberId;
         this.account = account;
@@ -36,74 +40,94 @@ public class Member {
         this.identity = identity;
     }
 
+    //getter id
     public Integer getMemberId() {
         return memberId;
     }
 
+    //setter id
     public void setMemberId(Integer memberId) {
         this.memberId = memberId;
     }
 
+    //getter account
     public String getAccount() {
         return account;
     }
 
+    //setter account
     public void setAccount(String account) {
         this.account = account;
     }
 
+    //getter password
     public String getPassword() {
         return password;
     }
 
+    //setter password
     public void setPassword(String password) {
         this.password = password;
     }
 
+    //getter real name
     public String getRealName() {
         return realName;
     }
 
+    //setter real name
     public void setRealName(String realName) {
         this.realName = realName;
     }
 
+    //getter gender
     public String getGender() {
         return gender;
     }
 
+    //setter gender
     public void setGender(String gender) {
         this.gender = gender;
     }
 
+    
+    //getter height
     public Double getHeight() {
         return height;
     }
 
+    //setter height
     public void setHeight(Double height) {
         this.height = height;
     }
 
+    
+    //getter weight
     public Double getWeight() {
         return weight;
     }
 
+    //setter weight
     public void setWeight(Double weight) {
         this.weight = weight;
     }
 
+    //getter bmi
     public Double getBMI() {
         return BMI;
     }
 
+    //setter bmi
     public void setBMI(Double BMI) {
         this.BMI = BMI;
     }
 
+    //getter fund
     public Double getFund() {
         return Fund;
     }
 
+    //setter fund
     public void setFund(Double fund) {
         Fund = fund;
     }
@@ -112,10 +136,12 @@ public class Member {
         return identity;
     }
 
+    //getter identity
     public void setIdentity(String identity) {
         this.identity = identity;
     }
 
+    //print method
     @Override
     public String toString() {
         return "Member{" +
