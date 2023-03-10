249039aa8c2e08ab40fb4e88429beca5a71847e5
Zhige Xia <122643293+Ericxiazhige@users.noreply.github.com>
2023-03-10 00:39:51 -0500

    Update gym.sql

--- gym.sql
+++ gym.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80023
  File Encoding         : 65001
 
- Date: 10/02/2023 20:05:13
+ Date: 09/03/2023 17:00:54
 */
 
 CREATE DATABASE `gym`;
@@ -24,11 +24,11 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `t_admin`;
 CREATE TABLE `t_admin`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'Administrator ID',
-  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Administrator account',
-  `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Administrator password',
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '管理员ID',
+  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '管理员账号',
+  `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '管理员密码',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_admin
@@ -56,22 +56,23 @@ CREATE TABLE `t_course`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `t_member`;
 CREATE TABLE `t_member`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'User ID',
-  `account` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User name',
-  `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'User Password',
-  `realName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Name',
-  `gender` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Gender',
-  `height` double NULL DEFAULT NULL COMMENT 'Height',
-  `weight` double NULL DEFAULT NULL COMMENT 'Body weight',
-  `BMI` double NULL DEFAULT NULL COMMENT 'BMI',
-  `Fund` double NULL DEFAULT NULL COMMENT 'FUND',
-  `VIP` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'VIP',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `memberId` int(0) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `account` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户密码',
+  `realName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '姓名',
+  `gender` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '性别',
+  `height` double NULL DEFAULT NULL COMMENT '身高',
+  `weight` double NULL DEFAULT NULL COMMENT '体重',
+  `BMI` double NULL DEFAULT NULL COMMENT '身体指数',
+  `Fund` double NULL DEFAULT NULL COMMENT '账户余额',
+  `identity` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '身份',
+  PRIMARY KEY (`memberId`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_member
 -- ----------------------------
+
 -- ----------------------------
 -- Table structure for t_schedule
 -- ----------------------------
@@ -105,4 +106,3 @@ CREATE TABLE `t_work`  (
 -- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
-
